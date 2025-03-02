6cb7896796160241527a8c3bd7922f78eac684e5
Axel Velasquez <yoloax11@gmail.com>
2025-03-01 20:17:30 -0800

    updated main an gitignores

--- backend/main.py
+++ backend/main.py
@@ -1,23 +1,9 @@
 from fastapi import FastAPI, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
-from processors.ImageProcessor import ImageProcessor
-from contextlib import asynccontextmanager
 
-# Initialize image processor globally
-image_processor = ImageProcessor()
+app = FastAPI()
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    """ Ensures proper startup and shutdown handling """
-    print("Starting Image Processor...")
-    yield  # Run FastAPI
-    print("Stopping Image Processor...")
-    image_processor.stop()
-
-app = FastAPI(lifespan=lifespan)
-
-# Enable CORS
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -28,19 +14,8 @@ app.add_middleware(
 
 @app.post("/upload_frame")
 async def upload_frame(file: UploadFile = File(...)):
-    """ Process uploaded image and return emotion analysis. """
-    frame_data = await file.read()
-    data = image_processor.process_image(frame_data)
-
-    if not data:
-        return {"status": "error", "message": "No face detected"}
-
-    predominant_emotion = data[0]["dominant_emotion"]
-    return {"status": "success", "emotion": predominant_emotion}
-
-@app.get("/")
-def index():
-    return {"message": "Welcome to the Video Processing App!"}
+  #TODO
+  return
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
