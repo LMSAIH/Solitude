31636d235acb6a716c5a179809e402f33acea67c
Axel Velasquez <yoloax11@gmail.com>
2025-03-02 21:10:48 -0800

    addedn= the expression contex provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ExpressionProvider } from './context/ExpressionContext.tsx'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ExpressionProvider>
+      <App />
+    </ExpressionProvider>
   </StrictMode>,
 )
